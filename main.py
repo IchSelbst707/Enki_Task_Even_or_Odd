0f464b543f3e05cc6d088fe88f817690fda7db50
Ingrid Camargo <103864402+IchSelbst707@users.noreply.github.com>
2022-10-18 19:30:39 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+"""Create a code who can say if a number is Even or Odd, basically 
+
+"""
+
+x = float(input("Write a number here: "))
+
+if x % 2 == 0:
+  print("The number is Even")
+else:
+  print("The number is Odd")
